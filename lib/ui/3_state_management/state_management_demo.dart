f9890f13708978a2f94af3c7a9e04710b9fac611
EnkhdalaiDavaasuren <2024012@miu.edu.mn>
2026-02-26 14:17:47 +0800

    Set State

--- lib/ui/3_state_management/state_management_demo.dart
+++ lib/ui/3_state_management/state_management_demo.dart
@@ -1,33 +1,68 @@
 import 'package:flutter/material.dart';
 
-class StateManagementDemo extends StatelessWidget {
+class StateManagementDemo extends StatefulWidget {
   const StateManagementDemo({super.key});
+
+  @override
+  State<StateManagementDemo> createState() => _StateManagementDemoState();
+}
+
+class _StateManagementDemoState extends State<StateManagementDemo> {
+  final colors = [
+    Colors.blue, 
+    Colors.yellow,
+    Colors.teal,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lime,
+    Colors.grey,
+    ];
+  
+  int colorIndex = 0;
+
+  int textIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Widgets and Layout')),
       body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Stack(
-              children: [
-                Container(
-                color: Colors.blue,
-                width: 200,
-                height: 200,  
-                ),
-                Text("0")
-              ]
-            ),
-            const SizedBox(height: 20),
-            OutlinedButton(onPressed: (){}, child: Text("Change Color")),
-            const SizedBox(height: 20),
-            OutlinedButton(onPressed: (){}, child: Text("Change Text")),
-            const SizedBox(height: 20),
-          ],
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Stack(
+                children: [
+                  Container(
+                    color: colors[colorIndex],
+                    width: 200,
+                    height: 200,  
+                    child: Center(child: Text("$textIndex", style: TextStyle(fontSize: 50),)),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              OutlinedButton(onPressed: ChangeColor, child: Text("Change Color")),
+              const SizedBox(height: 20),
+              OutlinedButton(onPressed: ChangeText, child: Text("Change Text")),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
       );
-}}
+    }
+
+    void ChangeColor() {
+      setState(() {
+        colorIndex++;
+        colorIndex = colorIndex % colors.length;
+      });
+    }
+
+    void ChangeText() {
+      setState(() {
+        textIndex++;
+      });
+    }
+}
