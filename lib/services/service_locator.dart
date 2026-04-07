1bb7d3f4b40fbb669d0d75deecfeb77c5c3e3934
EnkhdalaiDavaasuren <2024012@miu.edu.mn>
2026-04-07 13:34:54 +0800

    Sqlite widget

---
+++ lib/services/service_locator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_demo/services/auth/auth.dart';
+import 'package:flutter_demo/services/local_storage/local_storage.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerLazySingleton<LocalStorage>(() => LocalStorage());
+  getIt.registerLazySingleton<Auth>(() => Auth());
+}
