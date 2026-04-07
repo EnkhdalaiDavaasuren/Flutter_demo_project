5e46f920068568e5d7d237773d86173fce55d2d9
EnkhdalaiDavaasuren <2024012@miu.edu.mn>
2026-04-07 14:09:04 +0800

    Homework 2 - Database

--- lib/ui/demos/5_sqlite/database.dart
+++ lib/ui/demos/5_sqlite/database.dart
@@ -1,3 +1,7 @@
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DatabaseHelper {
   static const _dbName = 'my_database.db';
   static const _dbVersion = 1;
@@ -7,7 +11,27 @@ class DatabaseHelper {
   static const columnName = 'name';
   static const columnAge = 'age';
 
+  late Database _database;
+
   Future<void> init() async {
-    final folder = await getApplicationDocumentary();
+    final folder = await getApplicationDocumentsDirectory();
+    final path = join(folder.path, _dbName);
+    _database = await openDatabase(
+      path,
+      version: _dbVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    final sql =
+        '''
+CREATE TABLE $table (
+$columnId INTEGER PRIMARY KEY,
+$columnName TEXT NOT NULL,
+$columnAge INTEGER NOT NULL
+)
+''';
+    await db.execute(sql);
   }
 }
